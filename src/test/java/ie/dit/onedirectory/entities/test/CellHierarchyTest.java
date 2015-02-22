4531c06ef4f503ea74be308c20cbea699b79e6de
John Jerome Fleming <jjf.ucd@gmail.com>
2015-02-22 14:26:36 +0000

    ServiceLocalTest() removed from CellHierarchyTest

--- src/test/java/ie/dit/onedirectory/entities/test/CellHierarchyTest.java
+++ src/test/java/ie/dit/onedirectory/entities/test/CellHierarchyTest.java
@@ -103,22 +103,22 @@ public class CellHierarchyTest {
 		assertNull("Failed to Delete", shouldBeNull);
 	}
 
+	/*
 	@Test
 	public void ServiceLocalTest() throws Exception {
 
-		CellHierarchy ec = new CellHierarchy(13, INITIAL_HIER3, INITIAL_HIER32, INITIAL_HIER321);
-		
+		CellHierarchy ec = new CellHierarchy(13, INITIAL_HIER3, INITIAL_HIER32, INITIAL_HIER321);	
 		service.addCellHierarchy(ec);
 		
 		assertEquals("Failed to Add", service.getAllCellHierarchies().size(), 1);
 	
-		CellHierarchy ec2 = new CellHierarchy(17,UPDATED_HIER3, UPDATED_HIER32, UPDATED_HIER321);
+		CellHierarchy ec2 = new CellHierarchy(17, UPDATED_HIER3, UPDATED_HIER32, UPDATED_HIER321);
 		service.addCellHierarchy(ec2);
 		
 		assertEquals("Failed to Add", service.getAllCellHierarchies().size(), 2);
 	
 	}
-
+*/
 
 	private void clearData() throws Exception {
 		utx.begin();
